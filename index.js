cfdd31cb26ed7924392903ce09868e749cebbf65
Eyman Eghterafi <eghterafieyman@gmail.com>
2022-03-14 10:06:03 -0500

    commit tree cb78b6fc7f7bf25ba84a4fe47be56a8cd83b5f88

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-function submitData (name, email) {
-    fetch("http://localhost:3000/users"), {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        body: JSON.stringify({
-            name: "Eyman",
-            email: "eghterafieyman@gmail.com"
-        }),
+function submitData(name, email) {
+    console.log (name, email)
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+    "Content-Type": 'application/json',
+    "Accept": 'application/json'
     },
+    body: JSON.stringify({
+    name: name,
+    email: email
+        })
+    })
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (object) {
+        console.log(object);
+        // document.querySelector("body")
+        document.body.innerHTML = object.id
+      })
+      .catch(function (error) {
+        console.log(error.message);
+        document.body.innerHTML = error 
+      });
 }
+
+
+
+
 
